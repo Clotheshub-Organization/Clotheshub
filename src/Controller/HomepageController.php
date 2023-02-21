1431fb224efb1f2267b0e5a041c4d291874d4ce1
MinHieu17 <hieutmgcc200070@fpt.edu.vn>
2023-02-21 10:44:19 +0700

    commit1

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -14,53 +14,9 @@ class HomepageController extends AbstractController
     /**
      * @Route("Clotheshub/homepage", name="homepage")
      */
-    public function ShowHomepage(ProductRepository $repo): Response
+    public function ShowHomepage(): Response
     {
-        $product = $repo->findAll();
-        $brand = $repo->findAll();
         return $this->render('homepage/homepage.html.twig', [
-            'product' => $product,
-            'brand' => $brand
-        ]);
-    }
-
-    /**
-     * @Route("Clotheshub/about", name="homepageAbout")
-     */
-    public function ShowAbout(): Response
-    {
-        return $this->render('homepage/about.html.twig', [
-            'AboutController' => 'AboutController',
-        ]);
-    }
-
-    /**
-     * @Route("Clotheshub/contact", name="homepageContact")
-     */
-    public function ShowContact(): Response
-    {
-        return $this->render('homepage/contact.html.twig', [
-            'ContactController' => 'ContactController',
-        ]);
-    }
-
-      /**
-     * @Route("Clotheshub/cart", name="homepageCart")
-     */
-    public function ShowCart(): Response
-    {
-        return $this->render('homepage/cart.html.twig', [
-            'CartController' => 'CartController',
-        ]);
-    }
-
-     /**
-     * @Route("Clotheshub/product/{id}", name="product_detail", requirements={"id"="\d+"})
-     */
-    public function productDetail(Product $product): Response
-    {
-        return $this->render('product/productdetail.html.twig', [
-            'product' => $product
         ]);
     }
 }

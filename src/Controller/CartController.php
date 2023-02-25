b908edbe3d26cdec03584f297c8cf429269fd9cf
MinHieu1705 <124853962+MinHieu1705@users.noreply.github.com>
2023-02-25 14:24:17 +0700

    update order template and cart controller add order into order table

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -21,30 +21,28 @@ class CartController extends AbstractController
     {
         $quantity = $req->query->get('quantity');
         $user = $this->getUser();
-        $data[]=[
+        $data[] = [
             'id' => $user->getId()
         ];
         // $userid = $data[0]['id'];
         $cart = $cartRepo->findBy([
-            'product'=>$product,
-            'user'=>$user
+            'product' => $product,
+            'user' => $user
         ]);
 
-        if (count($cart) == 0){
+        if (count($cart) == 0) {
             $c = new Cart();
             $c->setProduct($product);
             $c->setUser($user);
             $c->setQuantity($quantity);
-        }
-
-        else {
+        } else {
             $c = $cartRepo->find($cart[0]->getId());
             $oldQuantity = $c->getQuantity();
             $newQuantity = $oldQuantity + $quantity;
             $c = $c->setQuantity($newQuantity);
         }
 
-        $cartRepo->add($c,true);
+        $cartRepo->add($c, true);
         return $this->redirectToRoute('showCart', [], Response::HTTP_SEE_OTHER);
     }
 
@@ -55,13 +53,13 @@ class CartController extends AbstractController
     public function showCart(CartRepository $repoCart, BrandRepository $reopBrand): Response
     {
         $user = $this->getUser();
-        $data[]=[
+        $data[] = [
             'id' => $user->getId()
         ];
         $userid = $data[0]['id'];
         $product = $repoCart->showCart($userid);
         $total = 0;
-        foreach ($product as $p){
+        foreach ($product as $p) {
             $total += $p['total'];
         }
         $brand = $reopBrand->findAll();
@@ -77,10 +75,44 @@ class CartController extends AbstractController
      * @Route("Clotheshub/cart/delete/{product}",name="deleteCart", requirements={"product"="\d+"})
      */
 
-     public function deleteBrandAction(CartRepository $repo, Cart $cart): Response
-     {
-         $repo->remove($cart, true);
-         return $this->redirectToRoute('showCart', [], Response::HTTP_SEE_OTHER);
-     }
- 
+    public function deleteBrandAction(CartRepository $repo, Cart $cart): Response
+    {
+        $repo->remove($cart, true);
+        return $this->redirectToRoute('showCart', [], Response::HTTP_SEE_OTHER);
+    }
+
+     /**
+     * @Route("Clotheshub/order/{user}", name="show_order", requirements={"id"="\d+"})
+     */
+    public function showOrder(OrderRepository $orderRepo, CartRepository $cartRepo, OrderdetailRepository $detailRepo, BrandRepository $brandRepo, ProductRepository $productRepo, EntityManagerInterface $en): Response
+    {
+        // insert into order table
+        $brand = $brandRepo->findAll();
+        $order = new Order();
+        // get user id
+        $user = $this->getUser();
+        $data[] = [
+            'id' => $user->getId()
+        ];
+        $userid = $data[0]['id'];
+
+        // set user id to order table
+        $order->setUser($user);
+
+        // get product id and quantity from cart
+        $product = $cartRepo->findCart($userid);
+
+        $total =0;
+        foreach($product as $p){
+            $total += $p['total'];
+        }
+        $order->setTotal($total);
+        $order->setDate(new \DateTime());
+        $orderRepo->add($order, true);
+
+        return $this->render('order/order.html.twig', [
+        ]);
+    }
+
+
 }
